cc5a7325588230b9f0be4129a78e271545d9a20b
PichetDotK <pckjoke@gmail.com>
2025-06-24 17:22:24 +0700

    commit

--- src/app/views/pages/patient/view-patient/view-patient.component.ts
+++ src/app/views/pages/patient/view-patient/view-patient.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
-
+import {  NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule,DatePipe  } from '@angular/common'; 
+import { LOCATION_DATA } from '../../../models/location-data';
+import {GoogleMap, MapMarker} from '@angular/google-maps';
+import { RouterModule, Router } from '@angular/router';
+import { ApiDataService } from '../../../../core/services/api-data.service';
+import Swal from 'sweetalert2';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 @Component({
   selector: 'app-view-patient',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbDatepickerModule,
+    ReactiveFormsModule,
+    CommonModule,
+    GoogleMap,
+    MapMarker,
+    RouterModule
+  ],
   templateUrl: './view-patient.component.html',
   styleUrl: './view-patient.component.scss'
 })
